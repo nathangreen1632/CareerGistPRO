601cb7f90207697d684aa9166e41073f1408619e
Nathan Green <ngreen1632@gmail.com>
2025-05-01 19:08:12 -0500

    Add analytics route to the server router
    Introduced a new '/analytics' route by integrating `analyticsRoutes` into the server's routing logic. This expands the server's capabilities to handle analytics-related requests.

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -5,12 +5,14 @@ import authRoutes from './authRoutes.js';
 import favoriteRoutes from './favoriteRoutes.js';
 import summaryRoutes from './summaryRoutes.js';
 import jobRoutes from './jobRoutes.js';
+import analyticsRoutes from "./analyticsRoutes";
 
 const router: Router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/favorites', favoriteRoutes);
 router.use('/summaries', summaryRoutes);
-router.use('/jobs', jobRoutes);   // Most important for fetching jobs
+router.use('/jobs', jobRoutes);
+router.use('/analytics', analyticsRoutes)
 
 export default router;
