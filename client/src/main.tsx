83c6ed258e9b02b7ad4c1bb711badabf16ac335b
Nathan Green <ngreen1632@gmail.com>
2025-05-07 16:30:50 -0500

    Add HelmetProvider and adjust provider hierarchy
    Introduced HelmetProvider for managing document head and reorganized the provider hierarchy to ensure correct context wrapping. This enhances scalability and improves document metadata handling.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
-import { Toaster } from "react-hot-toast";
+import { SessionProvider } from './context/SessionContext';
+import { HelmetProvider } from 'react-helmet-async';
+import { Toaster } from 'react-hot-toast';
 import './index.css';
-import {SessionProvider} from "./context/SessionContext";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <SessionProvider>
-        <React.Fragment>
+    <HelmetProvider>
+      <AuthProvider>
+        <SessionProvider>
           <App />
           <Toaster position="top-center" reverseOrder={false} />
-        </React.Fragment>
-      </SessionProvider>
-    </AuthProvider>
+        </SessionProvider>
+      </AuthProvider>
+    </HelmetProvider>
   </React.StrictMode>
-
 );
